9edd9f5abb8246812ad13159ddb341fd10813b43
zzyeah <541767316@qq.com>
2021-04-26 20:24:49 +0800

    获取渲染的dom&cssmodule&refs&信息弹出功能

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import './styles/global.less';
 import router from './router';
-
+import showMessage from './utils/showMessage';
+Vue.prototype.$showMessage = showMessage;
+// window.showMessage = showMessage;
+// showMessage('sadhoaisda');
 
 new Vue({
   // 渲染组件
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,35 @@
 <template>
-  <h1>首页</h1>
+  <div class="home-container" ref="container">
+    <h1>首页</h1>
+    <button @click="handleClick">click</button>
+  </div>
 </template>
 
 <script>
+// import { showMessage } from "@/utils";
 export default {
-
-}
+  methods: {
+    handleClick() {
+      this.$showMessage({
+        content: "评论成功",
+        type: "success",
+        duration: 2000,
+        container: this.$refs.container,
+        callback: function(){
+          console.log('完成');
+        }
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.home-container {
+  background: lightblue;
+  width: 300px;
+  height: 500px;
+  border: 1px solid;
+  margin: 50px auto;
+}
 </style>
