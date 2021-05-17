efca47f82280580f233ea4b055b612a639388fe8
zzyeah <541767316@qq.com>
2021-05-17 12:24:22 +0800

    事件总线

---
+++ src/eventBus.js
@@ -0,0 +1,39 @@
+// const listeners = {};
+
+// // 事件总线
+// export default {
+//     // 监听某一个事件
+//     $on(eventName, handler) {
+//         if (!listeners[eventName]) {
+//             listeners[eventName] = new Set();
+//         }
+//         listeners[eventName].add(handler);
+//     },
+//     // 取消监听
+//     $off(eventName, handler) {
+//         if (!listeners[eventName]) {
+//             return;
+//         }
+//         listeners[eventName].delete(handler);
+//     },
+//     // 触发事件
+//     $emit(eventName, ...args) {
+//         if (!listeners[eventName]) {
+//             return;
+//         }
+//         for (const handler of listeners[eventName]) {
+//             handler(...args);
+//         }
+//     }
+// }
+
+/**
+ * {
+ *   "event1": [handler1, handler2],
+ *   "event2": [handler1, handler2]
+ * }
+ */
+
+import Vue from 'vue';
+
+export default new Vue({});
